309e7eaa7b34bb18fad8453f011cc56ad66b945a
2goodAP <45424904+2goodAP@users.noreply.github.com>
2023-03-28 11:30:45 +0545

    Remove conflicting gtk config from home manager.

--- modules/user/desktop/default.nix
+++ modules/user/desktop/default.nix
@@ -1,32 +1,5 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./applications.nix
   ];
-
-  config = {
-    gtk = {
-      enable = true;
-      font = {
-        package = pkgs.noto-nerd-font;
-        name = "NotoSans Nerd Font";
-        size = 11;
-      };
-      iconTheme = {
-        package = pkgs.breeze-icons;
-        name = "Breeze";
-      };
-      theme = {
-        package = pkgs.libsForQt5.breeze-gtk;
-        name = "Breeze";
-      };
-    };
-
-    qt = {
-      enable = true;
-      style = {
-        package = pkgs.libsForQt5.breeze-qt5;
-        name = "Breeze";
-      };
-    };
-  };
 }
